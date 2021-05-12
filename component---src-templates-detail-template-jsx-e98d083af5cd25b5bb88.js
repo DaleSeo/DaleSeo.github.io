5a3822a67bd3b832cf0c4471e248a3f7d15fc7ce
Dale Seo <dale.seo@gmail.com>
2021-05-11 20:26:16 -0400

    Updates

--- component---src-templates-detail-template-jsx-e98d083af5cd25b5bb88.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{wgns:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),o=n.n(a),r=n("Zttt"),c=n("bpIZ");e.default=function(t){var e=t.pageContext.node,n=e.frontmatter.title;return o.a.createElement(r.a,{title:n},o.a.createElement(c.a,{node:e}))}}}]);
-//# sourceMappingURL=component---src-templates-detail-template-jsx-e98d083af5cd25b5bb88.js.map
